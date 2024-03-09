e667b9d4a5dd9cbc545172de02ec2b7990cf12e4
roboticsETF <robotics.etf.sa@gmail.com>
2024-03-09 21:08:10 +0100

    File 'Obstacles' is added in order to provide moving of (random) obstacles within 'perception_etflab' library.

---
+++ src/etf_modules/perception_etflab/include/environments/Obstacles.h
@@ -0,0 +1,39 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+#include <Eigen/Eigen>
+#include <yaml-cpp/yaml.h>
+
+#include <rclcpp/rclcpp.hpp>
+#include <pcl/point_cloud.h>
+#include <pcl_conversions/pcl_conversions.h>
+
+namespace perception_etflab
+{
+    class Obstacles
+    {
+    public:
+        Obstacles(const std::string &config_file_path);
+
+        void move(std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> &clusters);
+        void move(pcl::PointCloud<pcl::PointXYZRGB>::Ptr pcl);
+        bool isValid(const Eigen::Vector3f &pos, float vel);
+
+        inline int getPeriod() const { return period; }
+
+    private:
+        int num_obstacles;
+        Eigen::Vector3f dim;
+		float robot_max_vel;                                            // Maximal velocity for each obstacle
+        Eigen::Vector3f WS_center;								        // Workspace center point in [m]
+        float WS_radius; 										        // Workspace radius in [m]
+		float base_radius;
+		bool table_included;
+        int period; 						                            // Period in [ms]
+        float max_vel;                                                  // Maximal velocity of each obstacle in [m/s]
+		std::vector<Eigen::Vector3f> velocities; 		                // Velocity vector for each obstacle
+        std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> obstacles;  // Each obstacle represents a single cluster
+
+    };
+}
---
+++ src/etf_modules/perception_etflab/src/environments/Obstacles.cpp
@@ -0,0 +1,125 @@
+#include "environments/Obstacles.h"
+
+perception_etflab::Obstacles::Obstacles(const std::string &config_file_path)
+{
+    if (config_file_path.find("sim") == std::string::npos)
+		return;
+
+    // All these settings are only for simulated robot in order to be able to deal with simulated obstacles
+    std::string project_abs_path(__FILE__);
+	for (int i = 0; i < 4; i++)
+		project_abs_path = project_abs_path.substr(0, project_abs_path.find_last_of("/\\"));
+    
+    YAML::Node node = YAML::LoadFile(project_abs_path + config_file_path);
+
+    YAML::Node obs_node = node["random_obstacles"];
+    num_obstacles = obs_node["num"].as<int>();
+	max_vel = obs_node["max_vel"].as<float>();
+	for (int i = 0; i < 3; i++)
+		dim(i) = obs_node["dim"][i].as<float>();
+    period = obs_node["period"].as<int>();
+
+    YAML::Node robot_node = node["robot"];
+    table_included = robot_node["table_included"].as<bool>();
+    for (int i = 0; i < 3; i++)
+        WS_center(i) = robot_node["WS_center"][i].as<float>();
+
+    WS_radius = robot_node["WS_radius"].as<float>();
+    base_radius = std::max(robot_node["capsules_radius"][0].as<float>(), robot_node["capsules_radius"][1].as<float>()) + dim.norm();
+    robot_max_vel = robot_node["max_vel_first_joint"].as<float>();
+
+    // Initial settings for each obstacle
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Adding %d random obstacles...", num_obstacles);
+    Eigen::Vector3f pos, vel;
+    float r, fi, theta;
+    for (int i = 0; i < num_obstacles; i++)
+    {
+        r = float(rand()) / RAND_MAX * WS_radius;
+        fi = float(rand()) / RAND_MAX * 2 * M_PI;
+        theta = float(rand()) / RAND_MAX * M_PI;
+		pos.x() = WS_center.x() + r * std::cos(fi) * std::sin(theta);
+		pos.y() = WS_center.y() + r * std::sin(fi) * std::sin(theta);
+		pos.z() = WS_center.z() + r * std::cos(theta);
+
+        vel = Eigen::Vector3f::Random(3);
+        vel.normalize();
+		vel *= float(rand()) / RAND_MAX * max_vel;
+
+        if (!isValid(pos, vel.norm()))   
+            i--;
+        else
+        {
+            std::shared_ptr<pcl::PointCloud<pcl::PointXYZRGB>> cluster = std::make_shared<pcl::PointCloud<pcl::PointXYZRGB>>();
+            cluster->emplace_back(pcl::PointXYZRGB(pos.x(), pos.y(), pos.z()));
+            cluster->emplace_back(pcl::PointXYZRGB(pos.x() - dim.x()/2, pos.y() - dim.y()/2, pos.z() - dim.z()/2));
+            cluster->emplace_back(pcl::PointXYZRGB(pos.x() + dim.x()/2, pos.y() + dim.y()/2, pos.z() + dim.z()/2));
+            obstacles.emplace_back(cluster);
+            velocities.emplace_back(vel);
+
+            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "%d. Obstacle pos: (%f, %f, %f)", i, pos.x(), pos.y(), pos.z());
+        }
+    }
+}
+
+void perception_etflab::Obstacles::move(std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> &clusters)
+{
+    std::shared_ptr<pcl::PointCloud<pcl::PointXYZRGB>> cluster;
+    Eigen::Vector3f pos, vel;
+    float vel_intensity;
+    float delta_time = period / 1000.0;     // Conversion from [ms] to [s]
+
+    for (int i = 0; i < num_obstacles; i++)
+    {
+        cluster = obstacles[i];
+        pos = Eigen::Vector3f(cluster->points.front().x, cluster->points.front().y, cluster->points.front().z);
+        pos += velocities[i] * delta_time;
+        vel_intensity = velocities[i].norm();
+
+        if (!isValid(pos, vel_intensity))
+        {
+            // RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Invalid object position. Computing new velocity. ");
+            vel = Eigen::Vector3f::Random(3);
+            vel.normalize();
+            velocities[i] = vel_intensity * vel;
+            i--;
+        }
+        else
+        {
+            for (pcl::PointCloud<pcl::PointXYZRGB>::iterator point = cluster->begin(); point < cluster->end(); point++)
+            {
+                point->x += velocities[i].x() * delta_time;
+                point->y += velocities[i].y() * delta_time;
+                point->z += velocities[i].z() * delta_time;
+            }
+            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "%d. Obstacle pos: (%f, %f, %f)", i, pos.x(), pos.y(), pos.z());
+        }
+    }
+    clusters = obstacles;
+}
+
+void perception_etflab::Obstacles::move(pcl::PointCloud<pcl::PointXYZRGB>::Ptr pcl)
+{
+}
+
+// Check whether an object position 'pos' is valid when the object moves at 'vel' velocity
+bool perception_etflab::Obstacles::isValid(const Eigen::Vector3f &pos, float vel)
+{
+    float tol_radius = std::max(vel / robot_max_vel, base_radius);
+
+    if (table_included)
+    {
+        if ((pos - WS_center).norm() > WS_radius || pos.z() < 0 ||          // Out of workspace
+            pos.head(2).norm() < tol_radius && pos.z() < WS_center.z() ||   // Surrounding of robot base
+            (pos - WS_center).norm() < tol_radius)                          // Surrounding of robot base
+            return false;
+    }
+    else
+    {
+        if ((pos - WS_center).norm() > WS_radius ||                                                 // Out of workspace
+            pos.head(2).norm() < tol_radius && pos.z() < WS_center.z() && pos.z() > -base_radius || // Surrounding of robot base
+            (pos - WS_center).norm() < tol_radius)                                                  // Surrounding of robot base
+            return false;
+    }
+
+    return true;
+}
