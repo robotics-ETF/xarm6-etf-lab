8be176c36be258993b2ac825701cc82b0b09fe0a
Nermin Covic <ncovic1@etf.unsa.ba>
2024-03-29 23:56:07 +0100

    SIM_BRINGUP_LIBRARY renamed to SIM_BRINGUP

--- src/etf_modules/real_bringup/cmake/Findsim_bringup_library.cmake
+++ src/etf_modules/real_bringup/cmake/Findsim_bringup_library.cmake
@@ -6,7 +6,7 @@ get_filename_component(XARM6_ETF_LAB_PATH ${XARM6_ETF_LAB_PATH} DIRECTORY)
 get_filename_component(XARM6_ETF_LAB_PATH ${XARM6_ETF_LAB_PATH} DIRECTORY)
 
 # message("xarm6-etf-lab path: ${XARM6_ETF_LAB_PATH}")
-find_library(SIM_BRINGUP_LIBRARY
+find_library(SIM_BRINGUP
   NAMES sim_bringup_library
   PATHS ${XARM6_ETF_LAB_PATH}/build/sim_bringup/src
 )
@@ -21,8 +21,8 @@ set(SIM_BRINGUP_LIBRARY_INCLUDE_DIRS
   ${SIM_BRINGUP_PATH}/include/environments
 )
 
-if(SIM_BRINGUP_LIBRARY)
-  message(STATUS "Found sim_bringup_library at ${SIM_BRINGUP_LIBRARY}")
-else(SIM_BRINGUP_LIBRARY)
+if(SIM_BRINGUP)
+  message(STATUS "Found sim_bringup_library at ${SIM_BRINGUP}")
+else(SIM_BRINGUP)
   message(STATUS "Not found sim_bringup_library")
-endif(SIM_BRINGUP_LIBRARY)
+endif(SIM_BRINGUP)
