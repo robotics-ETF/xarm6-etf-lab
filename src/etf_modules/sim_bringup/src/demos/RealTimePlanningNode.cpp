ac84c10efcdc7f45b2f1c3a44a9c12375bec26b1
Nermin Covic <ncovic1@etf.unsa.ba>
2024-03-29 23:05:50 +0100

    Fixed bug in 'RealTimePlanningNode.cpp'.

--- src/etf_modules/sim_bringup/src/demos/RealTimePlanningNode.cpp
+++ src/etf_modules/sim_bringup/src/demos/RealTimePlanningNode.cpp
@@ -5,11 +5,11 @@ typedef planning::drbt::DRGBT DP;    // 'DP' is Dynamic Planner
 sim_bringup::RealTimePlanningNode::RealTimePlanningNode(const std::string &node_name, const std::string &config_file_path) : 
     BaseNode(node_name, config_file_path),
     AABB(config_file_path),
-    DP(scenario->getStateSpace(), scenario->getStart(), scenario->getGoal())
+    DP(Planner::scenario->getStateSpace(), Planner::scenario->getStart(), Planner::scenario->getGoal())
 {
     YAML::Node node { YAML::LoadFile(project_abs_path + config_file_path) };
 
-    AABB::setEnvironment(scenario->getEnvironment());
+    AABB::setEnvironment(Planner::scenario->getEnvironment());
     if (AABB::getMinNumCaptures() == 1)
         AABB::subscription = this->create_subscription<sensor_msgs::msg::PointCloud2>
             ("/bounding_boxes", 10, std::bind(&AABB::callback, this, std::placeholders::_1));
@@ -96,7 +96,7 @@ void sim_bringup::RealTimePlanningNode::planningCallback()
         if (Planner::isReady()) 
         {
             replan(DRGBTConfig::MAX_ITER_TIME - DP::getElapsedTime(DP::time_iter_start) - 2e-3);    // 2 [ms] is subtracted because of the following code lines
-            std::cout << "Elapsed time: " << DP::getElapsedTime(DP::time_iter_start, planning::TimeUnit::us) << " [us] \n";
+            std::cout << "Elapsed time: " << DP::getElapsedTime(DP::time_iter_start, planning::TimeUnit::ms) << " [ms] \n";
         }
         else
             std::cout << "PLANNER IS NOT READY! \n";
@@ -140,12 +140,14 @@ void sim_bringup::RealTimePlanningNode::replan(float max_planning_time)
         {
         case planning::RealTimeScheduling::FPS:
             RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Replanning with Fixed Priority Scheduling ");
-            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Trying to replan in %f [s]...", max_planning_time);
+            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Trying to replan in %f [ms]...", max_planning_time * 1e3);
             replanning_thread = std::thread([this, &result, &max_planning_time]() 
             {
                 result = Planner::solve(DP::q_target, DP::q_goal, max_planning_time);
             });
+    std::cout << "Elapsed time 1: " << DP::getElapsedTime(DP::time_iter_start, planning::TimeUnit::ms) << " [ms] \n";
             std::this_thread::sleep_for(std::chrono::microseconds(size_t(max_planning_time * 1e6)));
+    std::cout << "Elapsed time 2: " << DP::getElapsedTime(DP::time_iter_start, planning::TimeUnit::ms) << " [ms] \n";
             break;
         
         case planning::RealTimeScheduling::None:
@@ -159,7 +161,7 @@ void sim_bringup::RealTimePlanningNode::replan(float max_planning_time)
         if (result)
         {
             // First, interpolation of the predefined path is done in order that the horizon contains more states from the path
-            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "The path has been replanned in %f [s].", Planner::getPlanningTime());
+            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "The path has been replanned in %f [ms].", Planner::getPlanningTime() * 1e3);
             DP::acquirePredefinedPath(Planner::getPath());
             DP::clearHorizon(base::State::Status::Reached, false);
             DP::q_next = std::make_shared<planning::drbt::HorizonState>(DP::q_target, 0);
@@ -181,12 +183,12 @@ void sim_bringup::RealTimePlanningNode::replan(float max_planning_time)
 void sim_bringup::RealTimePlanningNode::computeTrajectory()
 {
     float time_offset { DP::updateCurrentState() };
-    // float time_current { DP::spline_next->getTimeCurrent() };
+    float time_current { DP::spline_next->getTimeCurrent() };
 
     Trajectory::clear();
-    // Trajectory::addPoint(time_offset, DP::spline_next->getPosition(time_current),
-    //                                   DP::spline_next->getVelocity(time_current),
-    //                                   DP::spline_next->getAcceleration(time_current));
+    Trajectory::addPoint(time_offset, DP::spline_next->getPosition(time_current),
+                                      DP::spline_next->getVelocity(time_current),
+                                      DP::spline_next->getAcceleration(time_current));
     Trajectory::addPoint(time_offset + spline_next->getTimeFinal(), DP::spline_next->getPosition(INFINITY));
     Trajectory::publish();
 
