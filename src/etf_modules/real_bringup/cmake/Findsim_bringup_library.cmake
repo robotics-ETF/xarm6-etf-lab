eacaa93b53430805becc6a5eae4b6bd667bb61ed
Nermin Covic <ncovic1@etf.unsa.ba>
2024-03-30 00:52:05 +0100

    Another try...

--- src/etf_modules/real_bringup/cmake/Findsim_bringup_library.cmake
+++ src/etf_modules/real_bringup/cmake/Findsim_bringup_library.cmake
@@ -6,9 +6,9 @@ get_filename_component(XARM6_ETF_LAB_PATH ${XARM6_ETF_LAB_PATH} DIRECTORY)
 get_filename_component(XARM6_ETF_LAB_PATH ${XARM6_ETF_LAB_PATH} DIRECTORY)
 
 # message("xarm6-etf-lab path: ${XARM6_ETF_LAB_PATH}")
-find_library(SIM_BRINGUP
+find_library(SIM_BRINGUP_LIBRARY
   NAMES sim_bringup_library
-  PATHS ${XARM6_ETF_LAB_PATH}/build/sim_bringup/src
+  PATHS ${XARM6_ETF_LAB_PATH}/build/sim_bringup_library/src
 )
 
 set(SIM_BRINGUP_PATH "${XARM6_ETF_LAB_PATH}/src/etf_modules/sim_bringup")
@@ -21,8 +21,8 @@ set(SIM_BRINGUP_LIBRARY_INCLUDE_DIRS
   ${SIM_BRINGUP_PATH}/include/environments
 )
 
-if(SIM_BRINGUP)
-  message(STATUS "Found sim_bringup_library at ${SIM_BRINGUP}")
-else(SIM_BRINGUP)
+if(SIM_BRINGUP_LIBRARY)
+  message(STATUS "Found sim_bringup_library at ${SIM_BRINGUP_LIBRARY}")
+else(SIM_BRINGUP_LIBRARY)
   message(STATUS "Not found sim_bringup_library")
-endif(SIM_BRINGUP)
+endif(SIM_BRINGUP_LIBRARY)
