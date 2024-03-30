82d53eff397afbbd46cf33d13a020cd99296b78c
Nermin Covic <ncovic1@etf.unsa.ba>
2024-03-30 14:18:03 +0100

    Fixed bug in 'AABB.cpp' function.

--- src/etf_modules/real_bringup/cmake/Findsim_bringup.cmake
+++ src/etf_modules/real_bringup/cmake/Findsim_bringup.cmake
@@ -6,7 +6,7 @@ get_filename_component(XARM6_ETF_LAB_PATH ${XARM6_ETF_LAB_PATH} DIRECTORY)
 get_filename_component(XARM6_ETF_LAB_PATH ${XARM6_ETF_LAB_PATH} DIRECTORY)
 
 # message("xarm6-etf-lab path: ${XARM6_ETF_LAB_PATH}")
-find_library(SIM_BRINGUP_LIBRARY
+find_library(SIM_BRINGUP
   NAMES sim_bringup
   PATHS ${XARM6_ETF_LAB_PATH}/build/sim_bringup/src
 )
@@ -14,15 +14,15 @@ find_library(SIM_BRINGUP_LIBRARY
 set(SIM_BRINGUP_PATH "${XARM6_ETF_LAB_PATH}/src/etf_modules/sim_bringup")
 
 # message("sim_bringup path: ${SIM_BRINGUP_PATH}")
-set(SIM_BRINGUP_LIBRARY_INCLUDE_DIRS
+set(SIM_BRINGUP_INCLUDE_DIRS
   ${SIM_BRINGUP_PATH}/include
   ${SIM_BRINGUP_PATH}/include/base
   ${SIM_BRINGUP_PATH}/include/demos
   ${SIM_BRINGUP_PATH}/include/environments
 )
 
-if(SIM_BRINGUP_LIBRARY)
-  message(STATUS "Found sim_bringup at ${SIM_BRINGUP_LIBRARY}")
-else(SIM_BRINGUP_LIBRARY)
+if(SIM_BRINGUP)
+  message(STATUS "Found sim_bringup at ${SIM_BRINGUP}")
+else(SIM_BRINGUP)
   message(STATUS "Not found sim_bringup")
-endif(SIM_BRINGUP_LIBRARY)
+endif(SIM_BRINGUP)
