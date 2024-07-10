601dd8f2487a24767c980c556fe05be777be9b62
Nermin Covic <ncovic1@etf.unsa.ba>
2024-07-10 14:44:13 +0200

    Removed unnecessary included folders.

--- src/etf_modules/aruco_calibration/aruco_ros/cmake/Findrpmpl_library.cmake
+++ src/etf_modules/aruco_calibration/aruco_ros/cmake/Findrpmpl_library.cmake
@@ -18,15 +18,8 @@ set(RPMPL_LIBRARY_INCLUDE_DIRS
   ${RPMPL_PATH}/include
   ${RPMPL_PATH}/include/state_spaces
   ${RPMPL_PATH}/include/state_spaces/real_vector_space
-  ${RPMPL_PATH}/include/planners
-  ${RPMPL_PATH}/include/planners/rrt
-  ${RPMPL_PATH}/include/planners/rbt
-  ${RPMPL_PATH}/include/planners/drbt
-  ${RPMPL_PATH}/include/planners/rbt_star
-  ${RPMPL_PATH}/include/planners/trajectory
   ${RPMPL_PATH}/include/robots
   ${RPMPL_PATH}/include/environments
-  ${RPMPL_PATH}/include/environments/objects
   ${RPMPL_PATH}/include/scenario
   ${RPMPL_PATH}/include/configurations
 )
