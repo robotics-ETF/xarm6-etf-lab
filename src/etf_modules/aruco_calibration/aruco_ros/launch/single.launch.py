4b85ac103488b12bbb5b4ba9a5ae97987a39d3db
Nihad <hodzicnihad10@gmail.com>
2024-07-24 12:34:03 +0000

    Fine tunning. Finished calibration for the right camera.

--- src/etf_modules/aruco_calibration/aruco_ros/launch/single.launch.py
+++ src/etf_modules/aruco_calibration/aruco_ros/launch/single.launch.py
@@ -44,7 +44,7 @@ def generate_launch_description():
     )
 
     marker_size_arg = DeclareLaunchArgument(
-        'marker_size', default_value='0.087',
+        'marker_size', default_value='0.151',
         description='Marker size in m. '
     )
 
