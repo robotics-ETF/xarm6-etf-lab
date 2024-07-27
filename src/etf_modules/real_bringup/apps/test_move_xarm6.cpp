3a1f8978070bea08af4b0a9d1c97d16632626f27
Nermin Covic <ncovic1@etf.unsa.ba>
2024-07-28 00:02:21 +0200

    Code refactoring...

--- src/etf_modules/real_bringup/apps/test_move_xarm6.cpp
+++ src/etf_modules/real_bringup/apps/test_move_xarm6.cpp
@@ -2,11 +2,12 @@
 
 int main(int argc, char *argv[])
 {
-	const std::string node_name = "move_xarm6_node";
-	const std::string config_file_path = "/real_bringup/data/move_xarm6_config.yaml";
+	const std::string node_name { "move_xarm6_node" };
+	const std::string config_file_path { "/real_bringup/data/move_xarm6_config.yaml" };
     
     rclcpp::init(argc, argv);
     rclcpp::spin(std::make_shared<real_bringup::MoveXArm6Node>(node_name, config_file_path));
     rclcpp::shutdown();
+
     return 0;
 }
--- src/etf_modules/real_bringup/apps/test_pick_and_place.cpp
+++ src/etf_modules/real_bringup/apps/test_pick_and_place.cpp
@@ -2,11 +2,12 @@
 
 int main(int argc, char *argv[])
 {
-    const std::string node_name = "pick_and_place_node";
-	const std::string config_file_path = "/real_bringup/data/pick_and_place_config.yaml";
+    const std::string node_name { "pick_and_place_node" };
+	const std::string config_file_path { "/real_bringup/data/pick_and_place_config.yaml" };
 
     rclcpp::init(argc, argv);
     rclcpp::spin(std::make_shared<real_bringup::PickAndPlaceNode>(node_name, config_file_path));
     rclcpp::shutdown();
+
     return 0;
 }
