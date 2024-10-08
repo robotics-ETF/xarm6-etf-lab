6f2709081a3648899ca59316585eef05fb6298be
Nermin Covic <ncovic1@etf.unsa.ba>
2024-10-08 19:36:11 +0200

    'RealTimeTaskPlanningNode' is implemented within real_bringup.

---
+++ src/etf_modules/real_bringup/include/real_demos/RealTimeTaskPlanningNode.h
@@ -0,0 +1,24 @@
+//
+// Created by nermin on 01.10.24.
+//
+
+#include "real_demos/TaskPlanningNode.h"
+#include "real_demos/RealTimePlanningNode.h"
+
+namespace real_bringup
+{
+    class RealTimeTaskPlanningNode : public real_bringup::TaskPlanningNode
+    {
+    public:
+        RealTimeTaskPlanningNode(const std::string &node_name, const std::string &config_file_path);
+
+    protected:
+        void planningCase() override;
+
+        std::shared_ptr<sim_bringup::RealTimePlanningNode> real_time_planning_node;
+        std::string dynamic_planner_config_file_path;
+        YAML::Node dynamic_node;
+        std::ofstream file_out;
+        std::thread dynamic_planner_thread;
+    };
+}
