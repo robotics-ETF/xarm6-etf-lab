ccaf3ba64594f1fcdbdbccc42fdbfefb155cda6a
Nermin Covic <ncovic1@etf.unsa.ba>
2025-12-24 13:17:12 +0100

    'trajectory_advance_time' updated to 0.050 [s]

--- src/etf_modules/real_bringup/apps/test_dynamic_planning.cpp
+++ src/etf_modules/real_bringup/apps/test_dynamic_planning.cpp
@@ -16,18 +16,18 @@ int main(int argc, char *argv[])
 	};
 
 	rclcpp::init(argc, argv);
-	std::shared_ptr<real_bringup::DynamicPlanningNode> dynamic_planning_node
-	{
-		std::make_shared<real_bringup::DynamicPlanningNode>(node_name, config_file_path, true)
-		// std::make_shared<real_bringup::DynamicPlanningNode>(node_name, config_file_path, true, "_test.log")
-	};
+	// std::shared_ptr<real_bringup::DynamicPlanningNode> dynamic_planning_node
+	// {
+	// 	std::make_shared<real_bringup::DynamicPlanningNode>(node_name, config_file_path, true)
+	// 	// std::make_shared<real_bringup::DynamicPlanningNode>(node_name, config_file_path, true, "_test.log")
+	// };
 
 	// Also, it is possible to call 'sim_bringup' here
-	// std::shared_ptr<sim_bringup::DynamicPlanningNode> dynamic_planning_node
-	// {
-	// 	// std::make_shared<sim_bringup::DynamicPlanningNode>(node_name, config_file_path, true)
-	// 	std::make_shared<sim_bringup::DynamicPlanningNode>(node_name, config_file_path, true, "_test.log")
-	// };
+	std::shared_ptr<sim_bringup::DynamicPlanningNode> dynamic_planning_node
+	{
+		// std::make_shared<sim_bringup::DynamicPlanningNode>(node_name, config_file_path, true)
+		std::make_shared<sim_bringup::DynamicPlanningNode>(node_name, config_file_path, true, "_test.log")
+	};
 	
 	// Works with a corresponding modification of the files 'xarm_ros_client.h' and 'xarm_ros_client.cpp' 
 	rclcpp::executors::MultiThreadedExecutor executor;
